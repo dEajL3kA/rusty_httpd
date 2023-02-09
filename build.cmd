d18c86625f614d278f2a6cf9c98cd7bbe1c94d71
dEajL3kA <Cumpoing79@web.de>
2023-02-09 18:54:11 +0100

    Some improvements to Windows build script.

--- build.cmd
+++ build.cmd
@@ -4,37 +4,45 @@ cd /d "%~dp0"
 set "PANDOC=C:\Program Files\Pandoc\pandoc.exe"
 set "INFO_ZIP=%CD%\etc\utilities\win32\zip.exe"
 
-for %%d in (out target) do (
+for %%d in (build target) do (
 	if exist %%~d\. ( rmdir /S /Q %%~d )
 )
 
-mkdir out
+for /F "tokens=* usebackq" %%i in (`start /B /WAIT "date" "%CD%\etc\utilities\win32\date.exe" "+%%Y-%%m-%%d"`) do (
+	set "BUILD_DATE=%%~i"
+)
+
+mkdir build
 
 for %%t in (i686 x86_64 aarch64) do (
 	echo ----------------------------------------------------------------
 	echo %%t-pc-windows-msvc
 	echo ----------------------------------------------------------------
 	cargo build --release --target=%%t-pc-windows-msvc
-	mkdir out\rusty_httpd.%%t-pc-windows-msvc
-	copy /Y /B target\%%t-pc-windows-msvc\release\rusty_httpd.exe out\rusty_httpd.%%t-pc-windows-msvc
-	attrib +R out\rusty_httpd.%%t-pc-windows-msvc\rusty_httpd.exe
-	mkdir out\rusty_httpd.%%t-pc-windows-msvc\public
-	copy /Y /B public\*.* out\rusty_httpd.%%t-pc-windows-msvc\public
-	attrib +R out\rusty_httpd.%%t-pc-windows-msvc\public\*.*
-	copy /Y /B LICENSE out\rusty_httpd.%%t-pc-windows-msvc\LICENSE.txt
-	attrib +R out\rusty_httpd.%%t-pc-windows-msvc\LICENSE.txt
-	"%PANDOC%" -f markdown -t html5 --metadata title="Rusty HTTP Server" -o out\rusty_httpd.%%t-pc-windows-msvc\README.html README.md
-	attrib +R out\rusty_httpd.%%t-pc-windows-msvc\README.html
+	mkdir build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc
+	copy /Y /B target\%%t-pc-windows-msvc\release\rusty_httpd.exe build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc
+	attrib +R build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\rusty_httpd.exe
+	mkdir build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\public
+	copy /Y /B public\*.* build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\public
+	attrib +R build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\public\*.*
+	copy /Y /B LICENSE build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\LICENSE.txt
+	attrib +R build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\LICENSE.txt
+	"%PANDOC%" -f markdown -t html5 --metadata title="Rusty HTTP Server" -o build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\README.html README.md
+	attrib +R build\rusty_httpd.%BUILD_DATE%.%%t-pc-windows-msvc\README.html
 )
 
 echo ----------------------------------------------------------------
 echo Create bundles...
 echo ----------------------------------------------------------------
-pushd out
+pushd build
 for /D %%t in (*) do (
 	"%INFO_ZIP%" -r -9 %%~nxt.zip %%~nxt
 	attrib +R %%~nxt.zip
 )
-
 popd
+
+echo.
+echo Completed.
+echo.
+
 pause
